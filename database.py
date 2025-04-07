115ac80960c315f71bc88e8eb8ad66cbddbcc1ae
Proloottricks <ritiksakhala17@gmail.com>
2025-04-07 12:09:45 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,20 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
 
-MONGO_URL = os.getenv("MONGO_URL")
-client = MongoClient(MONGO_URL)
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
 db = client["price_tracker"]
+collection = db["products"]
 
-def add_user(user_id):
-    if not db.users.find_one({"user_id": user_id}):
-        db.users.insert_one({"user_id": user_id})
+def insert_product(chat_id, url, title, price, image):
+    collection.update_one(
+        {"chat_id": chat_id, "url": url},
+        {"$set": {"title": title, "price": price, "image": image}},
+        upsert=True
+    )
 
-def add_product(user_id, url, details):
-    db.products.insert_one({
-        "user_id": user_id,
-        "url": url,
-        "title": details["title"],
-        "price": details["price"],
-        "image": details["image"],
-    })
-
-def get_user_products(user_id):
-    return list(db.products.find({"user_id": user_id}))
+def get_all_products(chat_id):
+    return list(collection.find({"chat_id": chat_id}, {"_id": 0}))
